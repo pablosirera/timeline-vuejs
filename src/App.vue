3afd8301c541f0017cba09b89573763b99b9f613
Pablo Sirera <psirera4@gmail.com>
2018-09-04 20:04:51 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Timeline
+      :timeline-items="timelineItems"
+      :message-when-no-items="messageWhenNoItems"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Timeline from './components/Timeline.vue'
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    HelloWorld
-  }
-};
+    Timeline
+  },
+  data: () => ({
+    messageWhenNoItems: 'No articles found.',
+    timelineItems: [
+      {
+        year: {
+          from: '2017',
+          to: '2018'
+        },
+        items: [
+          {
+            nameMonth: 'February',
+            title: 'Five',
+            description:
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde.'
+          },
+          {
+            nameMonth: 'January',
+            title: 'Five',
+            description:
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde.'
+          }
+        ]
+      },
+      {
+        year: {
+          from: '2016',
+          to: '2017'
+        },
+        items: [
+          {
+            nameMonth: 'July',
+            title: 'Five',
+            description:
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde.'
+          },
+          {
+            nameMonth: 'February',
+            title: 'Five',
+            description:
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde.'
+          },
+          {
+            nameMonth: 'January',
+            title: 'Five',
+            description:
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde.'
+          }
+        ]
+      }
+    ]
+  })
+}
 </script>
 
 <style lang="scss">
+* {
+  outline: none;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Timeline.vue
@@ -0,0 +1,76 @@
+<template>
+  <section class="timeline">
+    <div class="wrapper-timeline" v-if="hasItems">
+      <div
+        class="wrapper-item"
+        v-for="(timelineContent, timelineIndex) in timelineItems"
+        :key="timelineIndex">
+        <div class="section-year">
+          <p class="year">
+            {{ timelineContent.year.to }}
+          </p>
+          <p class="year">
+            {{ timelineContent.year.from }}
+          </p>
+        </div>
+        <TimelineItems :items="timelineContent.items"/>
+      </div>
+    </div>
+    <p v-else>{{ messageWhenNoItems }}</p>
+  </section>
+</template>
+
+<script>
+import TimelineItems from '../components/TimelineItems.vue'
+
+export default {
+  name: 'Timeline',
+  components: {
+    TimelineItems
+  },
+  props: {
+    timelineItems: {
+      type: Array,
+      default: () => []
+    },
+    messageWhenNoItems: {
+      type: String
+    }
+  },
+  computed: {
+    hasItems() {
+      return !!this.timelineItems.length
+    }
+  }
+}
+</script>
+
+<style lang="scss" scopped>
+.timeline {
+  width: 100%;
+  max-width: 500px;
+
+  .wrapper-timeline {
+    position: relative;
+
+    .wrapper-item {
+      display: grid;
+      grid-template-columns: 100px 1fr;
+      margin-bottom: 20px;
+    }
+
+    .section-year {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: flex-end;
+      padding: 15px;
+      font-weight: bold;
+      font-size: 20px;
+      .year {
+        margin: 0;
+      }
+    }
+  }
+}
+</style>

0b2ae8fbc5b8cc9afbe8855d26802f19745a9384
Seán <seanlynch@umail.ucc.ie>
2019-10-08 19:28:23 +0100

    nicer colours

--- src/App.vue
+++ src/App.vue
@@ -23,14 +23,14 @@ export default {
         title: 'Name',
         description:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde.',
-        color: '#000000'
+        color: '#2ecc71'
       },
       {
         from: new Date(2017, 8),
         title: 'Name',
         description:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum architecto dolor, vitae magnam voluptate accusantium assumenda numquam error mollitia, officia facere consequuntur reprehenderit cum voluptates, ea tempore beatae unde.',
-        color: '#ccc'
+        color: '#e74c3c'
       },
       {
         from: new Date(2016, 11),

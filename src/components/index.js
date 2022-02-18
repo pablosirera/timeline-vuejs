f1479a06f500cb575e2bb2cb44766bf90a6d66c0
reinerBa <4329883+reinerBa@users.noreply.github.com>
2022-02-18 22:03:05 +0100

    Install automatic in browser

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,12 @@
 import Timeline from './Timeline.vue'
 
 export default Timeline
+
 export function install(Vue, options) {
   Vue.component(Timeline.name, Timeline)
 }
+
+if (typeof window !== "undefined" && typeof window.Vue === "object") {
+  window.Vue.component(Timeline.name, Timeline)
+}
+
